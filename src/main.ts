10b697d9d6506807e067473d975c622350d39c9c
sillyll <2226415191@qq.com>
2023-07-23 22:21:46 +0800

    requestAnimationFrame

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import "@/views/three-demo3-物体缩放旋转.js";
+import "@/views/three-demo3-requestAnimationFrame.js";
 
 const app = createApp(App);
 app.use(ElementPlus);
