e227ea1c8f4d6a06e473989e9b41dd809a2047bb
sillyll <2226415191@qq.com>
2023-07-23 21:44:01 +0800

    控制物体移动

--- src/views/three-demo2-3物体移动.js
+++ src/views/three-demo2-3物体移动.js
@@ -27,7 +27,11 @@ const meterial = new THREE.MeshBasicMaterial({ color: "skyblue" }); //材质
 // 根据几何体和材质创建物体
 const cube = new THREE.Mesh(cubeGeometry, meterial);
 
-//将几何体添加到场景当中
+// 修改物体的位置
+// cube.position.set(5, 0, 0);
+cube.position.x = 3;
+
+// 将几何体添加到场景当中
 scene.add(cube);
 
 //初始化渲染器
@@ -49,6 +53,11 @@ scene.add(axesHelper);
 
 // 设置渲染函数
 function render() {
+  // 每一帧➕0.01
+  cube.position.x += 0.01;
+  if (cube.position.x > 5) {
+    cube.position.x = 0;
+  }
   renderer.render(scene, camera);
   // 渲染下一帧函数
   requestAnimationFrame(render);
